e60c0840c2243a257b40e16a443d8dda23f6af0f
Kendall Strautman <kendallstrautman@gmail.com>
2020-06-11 15:11:18 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { TinaProvider, TinaCMS } from 'tinacms';
+import Hero from './components/Hero';
 
 function App() {
+  const cms = new TinaCMS({
+    enabled: true,
+    sidebar: {
+      hidden: true,
+    },
+    toolbar: { hidden: false },
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <TinaProvider cms={cms}>
+      <Hero />
+    </TinaProvider>
   );
 }
 
---
+++ src/components/Hero.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/Hero.css';
+
+export default function Hero() {
+  return (
+    <div className="hero">
+      <h1>Suspended in a Sunbeam</h1>
+      <p>
+        Dispassionate extraterrestrial observer are creatures of the cosmos
+        courage of our questions inconspicuous motes of rock and gas a mote of
+        dust suspended in a sunbeam great turbulent clouds.
+      </p>
+    </div>
+  );
+}
