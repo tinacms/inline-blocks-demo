ebdb3528e6a2a02c31ed152064c92a7c954c5fe2
Kendall Strautman <kendallstrautman@gmail.com>
2020-06-17 14:04:16 -0700

    step 8: add inline blocks

--- src/Home.js
+++ src/Home.js
@@ -1,27 +1,37 @@
 import React from 'react';
 import { useForm } from 'tinacms';
-// 1. Import `InlineForm`
-import { InlineForm } from 'react-tinacms-inline';
-import { Hero } from './components/Hero';
+
+// 1. Import `InlineBlocks`
+import { InlineForm, InlineBlocks } from 'react-tinacms-inline';
+import { Hero, hero_template } from './components/Hero';
 import data from './data/data.json';
 
 export default function Home() {
+  // 2. Update initial values with 'blocks' data
   const formConfig = {
     id: './data/data.json',
     initialValues: {
-      hero: data.hero,
+      blocks: data.blocks,
     },
     onSubmit() {},
   };
 
-  const [pageData, form] = useForm(formConfig);
+  const [, form] = useForm(formConfig);
 
-  // 2. Wrap `InlineForm` around `Hero`, pass the form
   return (
     <div className="home">
-      <InlineForm form={form}>
-        <Hero data={pageData.hero} />
+      <InlineForm form={form} initialStatus="active">
+        {/* 3. Replace `Hero` with `InlineBlocks`*/}
+        <InlineBlocks name="blocks" blocks={HOME_BLOCKS} />
       </InlineForm>
     </div>
   );
 }
+
+// 4. Define the blocks for `InlineBlocks`
+const HOME_BLOCKS = {
+  hero: {
+    Component: Hero,
+    template: hero_template,
+  },
+};
