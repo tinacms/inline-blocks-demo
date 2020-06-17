932709001fed53837c723d848f27431bb5b77b5c
Kendall Strautman <kendallstrautman@gmail.com>
2020-06-17 13:29:56 -0700

    step 4: add inline fields

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,12 +1,19 @@
 import React from 'react';
+// 1. Import `InlineTextarea`
+import { InlineTextarea } from 'react-tinacms-inline';
 import '../styles/hero.css';
 
-export function Hero({ data }) {
+export function Hero() {
+  // 2. Replace `data` with Inline Fields
   return (
     <div className="hero">
       <div className="wrapper wrapper--narrow">
-        <h1>{data.headline}</h1>
-        <p>{data.subtext}</p>
+        <h1>
+          <InlineTextarea name="hero.headline" />
+        </h1>
+        <p>
+          <InlineTextarea name="hero.subtext" />
+        </p>
       </div>
     </div>
   );
