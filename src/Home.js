7aee86eaa5fa32e08344f8e21f7e41d0b8c2da60
Kendall Strautman <kendallstrautman@gmail.com>
2020-06-17 10:39:22 -0700

    chore: reset to step 1

--- src/Home.js
+++ src/Home.js
@@ -1,47 +1,11 @@
 import React from 'react';
-import { useForm } from 'tinacms';
-import { InlineForm, InlineBlocks } from 'react-tinacms-inline';
-import { Hero, hero_template } from './components/Hero';
-import { Images, images_template } from './components/Images';
-import { Paragraph, paragraph_template } from './components/Paragraph';
-import { FeaturesList, features_list_template } from './components/Features';
+import { Hero } from './components/Hero';
 import data from './data/data.json';
 
 export default function Home() {
-  const formConfig = {
-    id: './data/data.json',
-    initialValues: {
-      blocks: data.blocks,
-    },
-    onSubmit() {},
-  };
-
-  const [, form] = useForm(formConfig);
-
   return (
     <div className="home">
-      <InlineForm form={form} initialStatus="active">
-        <InlineBlocks name="blocks" blocks={HOME_BLOCKS} />
-      </InlineForm>
+      <Hero data={data.hero} />
     </div>
   );
 }
-
-const HOME_BLOCKS = {
-  hero: {
-    Component: Hero,
-    template: hero_template,
-  },
-  images: {
-    Component: Images,
-    template: images_template,
-  },
-  paragraph: {
-    Component: Paragraph,
-    template: paragraph_template,
-  },
-  features: {
-    Component: FeaturesList,
-    template: features_list_template,
-  },
-};
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,71 +1,13 @@
-import React from "react";
-import { InlineTextarea, BlocksControls } from "react-tinacms-inline";
-import "../styles/hero.css";
+import React from 'react';
+import '../styles/hero.css';
 
-export function Hero({ data, index }) {
+export function Hero({ data }) {
   return (
-    <BlocksControls
-      index={index}
-      focusRing={{ offset: 0 }}
-      insetControls={true}
-    >
-      <div
-        className="hero"
-        style={{
-          color: `${data.text_color || "#000"}`,
-          backgroundColor: `${data.background_color || "aliceblue"}`,
-          textAlign: `${data.align}`,
-          justifyContent: `${data.align === "left" ? "start" : data.align}`,
-        }}
-      >
-        <div className="wrapper wrapper--narrow">
-          <h1>
-            <InlineTextarea name="headline" focusRing={false} />
-          </h1>
-          <p>
-            <InlineTextarea name="subtext" focusRing={false} />
-          </p>
-        </div>
+    <div className="hero">
+      <div className="wrapper wrapper--narrow">
+        <h1>{data.headline}</h1>
+        <p>{data.subtext}</p>
       </div>
-    </BlocksControls>
+    </div>
   );
 }
-
-export const hero_template = {
-  label: "Hero",
-  defaultItem: {
-    headline: "Suspended in a Sunbeam",
-    subtext: "Dispassionate extraterrestrial observer",
-    background_color: "rgb(5, 30, 38)",
-    text_color: "#fffaf4",
-    align: "center",
-  },
-  fields: [
-    {
-      name: "background_color",
-      label: "Background Color",
-      component: "color",
-      widget: "block",
-      colors: [
-        "black",
-        "aliceblue",
-        "antiquewhite",
-        "aqua",
-        "azure",
-        "darkslategray",
-      ],
-    },
-    {
-      name: "text_color",
-      label: "Text Color",
-      component: "select",
-      options: ["black", "white"],
-    },
-    {
-      name: "align",
-      label: "Alignment",
-      component: "select",
-      options: ["center", "left"],
-    },
-  ],
-};
