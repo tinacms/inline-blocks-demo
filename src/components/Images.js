809d4af5b985e817b89756431d1d16fce9a8c43f
Kendall Strautman <kendallstrautman@gmail.com>
2020-06-22 11:17:53 -0700

    chore: remove unused image template

--- src/components/Images.js
+++ src/components/Images.js
@@ -31,60 +31,6 @@ function Images({ index }) {
   );
 }
 
-const images_template = {
-  label: 'Image Diptych',
-  defaultItem: {
-    _template: 'images',
-    left: {
-      src: '/ivan-bandura-unsplash-square.jpg',
-      alt: 'ocean',
-    },
-    right: {
-      src: '/martin-sanchez-unsplash-square.jpg',
-      alt: 'dunes',
-    },
-  },
-  fields: [
-    {
-      name: 'left.src',
-      label: 'Left-Hand Image',
-      component: 'image',
-      parse: (filename) => `/${filename}`,
-      uploadDir: () => '/',
-      previewSrc: (formValues, input) => {
-        // Assumes the block is only one level deep
-        const index = input.field.name.split('.')[1];
-        const currentBlockImage = formValues.blocks[index].left.src;
-        return currentBlockImage;
-      },
-      focusRing: false,
-    },
-    {
-      name: 'left.alt',
-      label: 'Left-Hand Image Alt Text',
-      component: 'text',
-    },
-    {
-      name: 'right.src',
-      label: 'Right-Hand Image',
-      component: 'image',
-      parse: (filename) => `/${filename}`,
-      uploadDir: () => '/',
-      previewSrc: (formValues, input) => {
-        const index = input.field.name.split('.')[1];
-        const currentBlockImage = formValues.blocks[index].right.src;
-        return currentBlockImage;
-      },
-      focusRing: false,
-    },
-    {
-      name: 'right.alt',
-      label: 'Right-Hand Image Alt Text',
-      component: 'text',
-    },
-  ],
-};
-
 export const imagesBlock = {
   Component: Images,
   template: {
