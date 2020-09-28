6fb0bb84c1793051798c28edd558726e89d516af
Kendall Strautman <kendallstrautman@gmail.com>
2020-09-28 14:06:55 -0600

    feat: adds dummy media store

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { TinaProvider, TinaCMS } from 'tinacms';
 import Home from './Home';
+import DummyMediaStore from './mediaStore';
 
 function App() {
   const cms = new TinaCMS({
     enabled: true,
     toolbar: true,
+    media: new DummyMediaStore(),
   });
 
   return (
